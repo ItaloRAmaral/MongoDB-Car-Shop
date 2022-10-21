5dbdef1f6e465f93a5ec7a21f50336f3bd8864f7
Italo Amaral <italorockamaral@gmail.com>
2022-10-21 15:31:19 -0300

    REAFACTOR: IVehicle interface

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,11 +1,28 @@
 import { z } from 'zod';
 
 const VehicleZodSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().positive().gte(1900).lte(2022),
-  color: z.string().min(3),
+  model: z.string({
+    required_error: 'Model is required',
+    invalid_type_error: 'Model must be a string',
+  }).min(3, { message: 'Model must be at least 3 characters' }),
+
+  year: z.number({
+    required_error: 'Year is required',
+    invalid_type_error: 'Year must be a number',
+  }).gte(1900, { message: 'Year must be more than 1900 or equal' })
+    .lte(2022, { message: 'Year must be less than 2022 or equal' })
+    .int({ message: 'Year must be an integer' }),
+
+  color: z.string({
+    required_error: 'Color is required',
+    invalid_type_error: 'Color must be a string',
+  }).min(3, { message: 'Color must be at least 3 characters' }),
+
   status: z.boolean().optional(),
-  buyValue: z.number().positive(),
+  buyValue: z.number({
+    required_error: 'Buy value is required',
+    invalid_type_error: 'Buy value must be a number',
+  }).int({ message: 'Buy value must be an integer' }),
 });
 
 type IVehicle = z.infer<typeof VehicleZodSchema>;
