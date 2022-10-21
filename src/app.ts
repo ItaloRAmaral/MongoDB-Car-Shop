ff846bc82fb92c888362a895d877d69749a4f6f2
Italo Amaral <italorockamaral@gmail.com>
2022-10-21 16:06:28 -0300

    FEAT: using errorHandler in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import errorHandler from './middlewares/errorMiddleware';
 
 const app = express();
+app.use(express.json());
+
+app.use(errorHandler);
 
 export default app;
